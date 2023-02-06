4dd8e29b9b12ab69705bfdcf71aefccf5a68ed4d
Ryotaro.Yabuzuka <ryotaro.yabuzuka@ibm.com>
2023-02-06 21:56:06 +0900

    作成

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func buttonTapped(sender: UIButton) {
+        let url = URL(fileURLWithPath: Bundle.main.bundlePath).appendingPathComponent("sample.mp3")
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.delegate = self
+            player.play()
+        } catch {
+            print("Failed!")
+        }
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        print("Finished")
+    }
+    
 }
 
